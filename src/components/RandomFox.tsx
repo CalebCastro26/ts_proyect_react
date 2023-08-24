c56bc667246a72778c4befe5724ea8501e450119
Caleb Castro <ccastrop@csticorp.biz>
2023-08-23 20:42:37 -0500

    cambios de inicio

---
+++ src/components/RandomFox.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+//import type { FunctionComponent, FC } from 'react'
+
+export const RandomFox = (): JSX.Element => {
+  return (
+    <img />
+  )
+}
+
+// export const RandomFox: FunctionComponent = () => {
+//   return (
+//     <img />
+//   )
+// }
+// export const RandomFox: FC = (): JSX.Element => {
+//   return (
+//     <img />
+//   )
+
+// }
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Home({ }: Props) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div >
+  )
+}
